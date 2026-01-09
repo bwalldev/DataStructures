73e37813b3f303cd141955fddc0b4d23d893d281
Brandon Wall <brandon.wall.dev@gmail.com>
2026-01-09 13:57:24 -0800

    Added methods to DoublyLinkedList.h
    Created the constructor, size(), and clear().

--- DoublyLinkedList/DoublyLinkedList.h
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -6,7 +6,33 @@ template <typename T>
 class DoublyLinkedList {
     private:
         int length;
+        Node<T>* head;
+        Node<T>* tail;
     public:
-        //DoublyLinkedList();
+        DoublyLinkedList() : length(0), head(nullptr), tail(nullptr) {}
         //~DoublyLinkedList();
+        int size();
+        void clear();
 };
+
+// Size: Returns the number of elements currently in the doubly linked list
+template <typename T>
+int DoublyLinkedList<T>::size() {
+    return length;
+}
+
+// Clear: Removing all the elements from the doubly linked list
+template <typename T>
+void DoublyLinkedList<T>::clear() {
+    Node<T>* curr = head;
+
+    while (curr) {
+        Node<T>* tmp = curr->next;
+        delete curr;
+        curr = tmp;
+    }
+
+    head = nullptr;
+    tail = nullptr;
+    length = 0;
+}
