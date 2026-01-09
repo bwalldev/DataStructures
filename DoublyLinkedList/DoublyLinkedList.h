215f1d56523f8b3802bffef9a4a81a2833fcbbe6
Brandon Wall <brandon.wall.dev@gmail.com>
2026-01-09 13:38:30 -0800

    Created DoublyLinkedList.h

---
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -0,0 +1,12 @@
+// Doubly Linked List implementation
+
+#include "Node.h"
+
+template <typename T>
+class DoublyLinkedList {
+    private:
+        int length;
+    public:
+        //DoublyLinkedList();
+        //~DoublyLinkedList();
+};
