f51ad20829547dcd536352e242bcbad20af58b5b
Brandon Wall <brandon.wall.dev@gmail.com>
2026-06-05 12:35:56 -0700

    Updated driver.cpp

--- DoublyLinkedList/driver.cpp
+++ DoublyLinkedList/driver.cpp
@@ -5,6 +5,10 @@ int main(int argc, char *args[])
 {
     DoublyLinkedList<std::string> bands;
 
+    std::cout << "\nHow many items are in the list? " << bands.getSize() << "\n\n";
+
+    std::cout << "*Populating list with bands*\n\n";
+
     bands.pushBack("Belmont");
     bands.pushFront("The Starting Line");
     bands.pushFront("Blink 182");
@@ -13,6 +17,16 @@ int main(int argc, char *args[])
 
     bands.printForward();
     bands.printBackward();
+
+    std::cout << "\nHow many items are in the list? " << bands.getSize() << "\n\n";
+    std::cout << "Does Sum 41 exist in the list? " << (bands.exists("Sum 41") ? "Yes\n" : "No\n");
+    std::cout << "Does Underbrook exist in the list? " << (bands.exists("Underbrook") ? "Yes\n\n" : "no\n\n");
+
+    std::cout << "*Clearing list*\n\n";
+
+    bands.clear();
+
+    std::cout << "How many items are in the list? " << bands.getSize();
     
     return 0;
 }
